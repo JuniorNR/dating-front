8ef4d0b5c9867637a0b4c996f34764ffd5bc7f1e
Oleg Kudelkin <tryhard.work@mail.ru>
2026-02-28 18:06:57 +0300

    feat: customize scrollbar

--- src/widgets/Main/ui/Main.tsx
+++ src/widgets/Main/ui/Main.tsx
@@ -3,7 +3,7 @@ import { MainProps } from '../model/main.types';
 
 export const Main: FC<MainProps> = ({ children }) => {
 	return (
-		<main className="min-h-[calc(100vh-50px-25px)] max-h-[calc(100vh-50px-25px)] overflow-y-auto overflow-x-hidden grow w-full max-w-screen-2xl mx-auto p-2">
+		<main className="min-h-[calc(100vh-50px-25px)] max-h-[calc(100vh-50px-25px)] overflow-y-auto overflow-x-hidden grow w-full py-2 px-[calc(0.5rem+max(0px,(100vw-96rem)/2))]">
 			{children}
 		</main>
 	);
